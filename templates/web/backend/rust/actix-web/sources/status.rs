f0c8df7bba1af62bc65562bd05f6c37ee5442d63
doanmihh153 <doanmihh1533@gmail.com>
2026-07-15 04:14:31 +0700

    fix: suppress dead_code warnings in Rust templates
    - axum middleware.rs: remove unused IntoResponse import
    - axum/actix config.rs: #[allow(dead_code)] on framework field
    - axum/actix status.rs: #[allow(dead_code)] on Status + get_status()

--- templates/web/backend/rust/actix-web/sources/status.rs
+++ templates/web/backend/rust/actix-web/sources/status.rs
@@ -1,11 +1,13 @@
 use serde::Serialize;
 
 #[derive(Serialize)]
+#[allow(dead_code)]
 pub struct Status {
     pub service: String,
     pub version: String,
 }
 
+#[allow(dead_code)]
 pub fn get_status() -> Status {
     Status {
         service: "{{project_name}}".to_string(),
