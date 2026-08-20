cbc3875253ef471c1b63567f3d389c2568ca7cf5
doanmihh153 <doanmihh1533@gmail.com>
2026-08-20 16:19:24 +0700

    chore: remove demo artifact cli/game-demo

--- cli/game-demo/src/systems/movement.rs
+++
@@ -1,9 +0,0 @@
-use bevy::prelude::*;
-
-use crate::components::Player;
-
-pub fn spin(mut q: Query<(&mut Transform, &Player)>, time: Res<Time>) {
-    for (mut t, player) in &mut q {
-        t.rotate_z(player.spin_speed * time.delta().as_secs_f32());
-    }
-}
